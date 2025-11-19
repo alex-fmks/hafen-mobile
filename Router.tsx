c0b29b0ae3677a8e5f55b60b39a0cf89d414848b
Alexander <90181605+alex-fmks@users.noreply.github.com>
2025-11-19 13:38:21 +0100

    install icon lib for react native

--- Router.tsx
+++ Router.tsx
@@ -3,12 +3,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "./src/Screens/Home";
+import {
+  HomeIcon,
+  PhotoIcon,
+  BuildingStorefrontIcon,
+  MapPinIcon,
+} from "react-native-heroicons/solid";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator initialRouteName="Home">
+  <Tab.Navigator
+    initialRouteName="Home"
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => {
+        let iconName;
+        if (route.name === "Home") {
+          iconName = <HomeIcon fill={focused ? "#3f1ec1ff" : "gray"} />;
+        } else if (route.name === "About") {
+          iconName = (
+            <BuildingStorefrontIcon fill={focused ? "#3f1ec1ff" : "gray"} />
+          );
+        } else if (route.name === "Instagram") {
+          iconName = <PhotoIcon fill={focused ? "#3f1ec1ff" : "gray"} />;
+        } else if (route.name === "Location") {
+          iconName = <MapPinIcon fill={focused ? "#3f1ec1ff" : "gray"} />;
+        }
+        return iconName;
+      },
+      tabBarActiveTintColor: "#3f1ec1ff",
+      tabBarInactiveTintColor: "gray",
+    })}
+  >
     <Tab.Screen name="Home" component={Home} options={{ title: "Home" }} />
     <Tab.Screen name="About" component={Home} options={{ title: "Über uns" }} />
     <Tab.Screen
